fd9adb7a98ebe52182214680a797a8fb55352dad
SomiyaNamdeo <namdeosomiya2005@gmail.com>
2026-08-30 21:41:23 +0530

    Cleaned the strucuture

--- fix_paths.py
+++
@@ -1,8 +0,0 @@
-﻿with open('README.md', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('src="frontend/', 'src="./frontend/')
-content = content.replace('src="docs/', 'src="./docs/')
-
-with open('README.md', 'w', encoding='utf-8') as f:
-    f.write(content)
