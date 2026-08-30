9d52f004ca3b98c7b98fd22f23b516a9b6c6319f
SomiyaNamdeo <namdeosomiya2005@gmail.com>
2026-08-30 15:49:42 +0530

    Allow production frontend in CORS

--- app/main.py
+++ app/main.py
@@ -32,7 +32,12 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        FRONTEND_URL,
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://forge-beta-gilt-16.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
